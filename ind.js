b1aa5c0956cd59112626f7814847414f11bb44e1
swediRoben <farajampenda43@gmail.com>
2023-11-02 18:58:50 +0200

    change

---
+++ ind.js
@@ -0,0 +1,169 @@
+let input = document.getElementById('input')
+let span = document.querySelector('span')
+
+sessionStorage.setItem("token", "bearer hdhfhffbbddsddd admin")
+
+$(".translate").change(function() {
+    var langue = $(this).val();
+    sessionStorage.setItem("lang", langue)
+    sessionStorage.setItem("langue", sessionStorage.getItem("lang"))
+})
+
+var index = {
+    '0': {
+        "titre": "titre",
+        "img": {
+            "1": "url"
+        },
+        "descritiption": "descr",
+        "langue": 0
+    },
+    '1': {
+        "titre": "titre",
+        "img": {
+            "1": "url"
+        },
+        "descritiption": "descr",
+        "langue": 0
+    },
+    '2': {
+        "titre": "titre",
+        "img": {
+            "1": "url"
+        },
+        "descritiption": "descr",
+        "langue": 0
+    },
+    '3': {
+        "titre": "titre",
+        "img": {
+            "1": "url"
+        },
+        "descritiption": "descr",
+        "langue": 0
+    },
+}
+
+
+var menuApropos = {
+    '0': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '1': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '2': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '3': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+
+    }
+}
+
+var aproposDesciption = {
+    '0': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '1': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '2': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    },
+    '3': {
+        '1': {
+            'key': '1',
+            'description': 'desc'
+        },
+    }
+}
+
+var contanct = {
+    '0': {
+        'titre': 'titre',
+        'description': 'desc'
+    },
+    '1': {
+        'titre': 'titre',
+        'description': 'desc'
+    },
+    '2': {
+        'titre': 'titre',
+        'description': 'desc'
+    },
+    '3': {
+        'titre': 'titre',
+        'description': 'desc'
+    }
+}
+
+
+$(function() {
+    $(".translate").click(function() {
+        var langue = $(this).attr('id');
+        $(".langs").each(function(index, element) {
+            $(this).text(menuApropos[langue][$(this).attr('key')]);
+            $(this).text(menuApropos[langue]['pubsous'][$(this).attr('key')]);
+        });
+    });
+})
+
+$(function() {
+    $(".translate").change(function() {
+        // var langue = $(this).val();
+        var langue = sessionStorage.getItem("langue");
+        console.log(langue)
+        $(".langs").each(function(index, element) {
+            $(this).text(menuApropos[langue][$(this).attr('key')]);
+            $(this).text(menuApropos[langue]['pubsous'][$(this).attr('key')]);
+            $(this).text(index[langue][$(this).attr('key')]);
+            $(this).text(index[langue]['pubsousmanu'][$(this).attr('key')]);
+            $(this).text(index[langue]['projet'][$(this).attr('key')]);
+        });
+    });
+})
+
+
+
+var langSession = sessionStorage.getItem("langue");
+if (langSession !== null) {
+    $(".langs").each(function(index, element) {
+        $(this).text(menuApropos[langSession][$(this).attr('key')]);
+        $(this).text(menuApropos[langSession]['pubsous'][$(this).attr('key')]);
+        $(this).text(index[langSession][$(this).attr('key')]);
+        $(this).text(index[langSession]['pubsousmanu'][$(this).attr('key')]);
+        $(this).text(index[langSession]['projet'][$(this).attr('key')]);
+    });
+}
+
+
+function langue() {
+    var langSession = sessionStorage.getItem("langue");
+    if (langSession == 0) {
+        return "fr"
+    } else {
+        return "en"
+    }
+}
--- js/methode/index.js
+++ js/methode/index.js
@@ -3,74 +3,102 @@ $(function() {
     var langSession = sessionStorage.getItem("langue");
 
 
-    if (langSession !== null) {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langSession,
-            dataType: "json",
-            headers: {
-                "Accept-Language": lang,
-                "token": ""
-            },
-            success: function(response) {
-                $.each(response.data.contents, function(i, data) {
-                    $(".act1").attr("src", data.urlfile);
-                    $(".titre").text(data.titre);
-                    $(".act2").attr("src", data.urlfile);
-                    $(".date").text(data.date);
-                    $(".act3").attr("src", data.urlfile);
-                    $(".mail").text(data.mail);
+    // if (langSession !== null) {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langSession,
+    //         dataType: "json",
+    //         headers: {
+    //             "Accept-Language": lang,
+    //             "token": ""
+    //         },
+    //         success: function(response) {
+    //             $.each(response.data.contents, function(i, data) {
+    //                 $(".act1").attr("src", data.urlfile);
+    //                 $(".titre").text(data.titre);
+    //                 $(".act2").attr("src", data.urlfile);
+    //                 $(".date").text(data.date);
+    //                 $(".act3").attr("src", data.urlfile);
+    //                 $(".mail").text(data.mail);
 
-                });
-                console.log(response)
-            }
-        });
-    }
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langSession,
-        dataType: "json",
-        headers: {
-            "Accept-Language": lang,
-            "token": ""
-        },
-        success: function(response) {
-            $.each(response.data.contents, function(i, data) {
-                $(".act1").attr("src", data.urlfile);
-                $(".titre").text(data.titre);
-                $(".act2").attr("src", data.urlfile);
-                $(".date").text(data.date);
-                $(".act3").attr("src", data.urlfile);
-                $(".mail").text(data.mail);
+    //             });
+    //             console.log(response)
+    //         }
+    //     });
+    // }
+    // $.ajax({
+    //     type: "GET",
+    //     url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langSession,
+    //     dataType: "json",
+    //     headers: {
+    //         "Accept-Language": lang,
+    //         "token": ""
+    //     },
+    //     success: function(response) {
+    //         $.each(response.data.contents, function(i, data) {
+    //             $(".act1").attr("src", data.urlfile);
+    //             $(".titre").text(data.titre);
+    //             $(".act2").attr("src", data.urlfile);
+    //             $(".date").text(data.date);
+    //             $(".act3").attr("src", data.urlfile);
+    //             $(".mail").text(data.mail);
 
-            });
-            console.log(response)
+    //         });
+    //         console.log(response)
+    //     }
+    // });
+
+
+    // $(".translate").change(function() {
+    //     // var langue = $(this).val();
+    //     var langue = sessionStorage.getItem("langue");
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langue,
+    //         dataType: "json",
+    //         headers: {
+    //             "Accept-Language": lang,
+    //             "token": ""
+    //         },
+    //         success: function(response) {
+    //             $.each(response.data.contents, function(i, data) {
+    //                 $(".act1").attr("src", data.urlfile);
+    //                 $(".titre").text(data.titre);
+    //                 $(".act2").attr("src", data.urlfile);
+    //                 $(".date").text(data.dateupdate);
+    //                 $(".act3").attr("src", data.urlfile);
+
+    //             });
+    //             console.log(response)
+    //         }
+    //     });
+    // });
+
+
+    $("#profile-form").submit(function(event) {
+        event.preventDefault();
+        var formData = new FormData();
+        var files = $('#files')[0].files;
+        for (var i = 0; i < files.length; i++) {
+            formData.append('files[]', files[i]);
         }
-    });
-
-
-    $(".translate").change(function() {
-        // var langue = $(this).val();
-        var langue = sessionStorage.getItem("langue");
         $.ajax({
-            type: "GET",
-            url: "http://localhost:3031/publication/?type=0&typeFichier=0&langue=" + langue,
-            dataType: "json",
+            url: 'http://localhost:3031/publication/',
+            type: 'POST',
+            data: JSON.stringify(formData),
+            processData: false,
+            contentType: "application/json",
             headers: {
                 "Accept-Language": lang,
                 "token": ""
             },
             success: function(response) {
-                $.each(response.data.contents, function(i, data) {
-                    $(".act1").attr("src", data.urlfile);
-                    $(".titre").text(data.titre);
-                    $(".act2").attr("src", data.urlfile);
-                    $(".date").text(data.dateupdate);
-                    $(".act3").attr("src", data.urlfile);
-
-                });
-                console.log(response)
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
             }
         });
     });
+
 })
